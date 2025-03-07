fd02a50f1b6762ba3b3e429610e6d3dae5feaeac
algha3 <anaufalhz03@gmail.com>
2025-03-08 02:14:17 +0700

    add block scroll, audio, section doa, section pengantin

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,13 @@ const images = [
 const bgfull = "/Pict1.jpg";
 
 export default function App() {
+  const [isScrollAllowed, setIsScrollAllowed] = useState(false);
   const inviteSectionRef = useRef(null);
   const eventDate = new Date("2025-05-03T09:00:00").getTime();
   const [bgImage, setBgImage] = useState(images[0]);
   const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+  const [isPlaying, setIsPlaying] = useState(true);
+  const audioRef = useRef(null);
 
   const [posts, setPosts] = useState([]);
   const [post, setPost] = useState({
@@ -84,6 +87,21 @@ export default function App() {
   }, []);
 
   useEffect(() => {
+    if (!isScrollAllowed) {
+      // Blokir scroll
+      document.body.classList.add("no-scroll");
+    } else {
+      // Izinkan scroll
+      document.body.classList.remove("no-scroll");
+    }
+
+    // Bersihkan event listener saat komponen di-unmount
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [isScrollAllowed]);
+
+  useEffect(() => {
     const countdown = setInterval(() => {
       setTimeLeft(getTimeLeft());
     }, 1000);
@@ -104,8 +122,41 @@ export default function App() {
   }
 
   const handleScroll = () => {
+    setIsScrollAllowed(true); // Izinkan scroll
     inviteSectionRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
+  const togglePlayPause = () => {
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleUserInteraction = () => {
+      if (audioRef.current) {
+        audioRef.current.muted = false; // Unmute audio
+        audioRef.current.play(); // Play audio
+        setIsPlaying(true);
+        // Hapus event listener setelah interaksi pertama
+        document.removeEventListener("click", handleUserInteraction);
+        document.removeEventListener("scroll", handleUserInteraction);
+      }
+    };
+
+    // Tambahkan event listener untuk interaksi pengguna
+    document.addEventListener("click", handleUserInteraction);
+    document.addEventListener("scroll", handleUserInteraction);
+
+    return () => {
+      document.removeEventListener("click", handleUserInteraction);
+      document.removeEventListener("scroll", handleUserInteraction);
+    };
+  }, []);
 
   return (
     <div
@@ -116,6 +167,60 @@ export default function App() {
         className="relative flex-grow max-w-xl mx-auto border-l border-r border-white"
         style={{ backgroundImage: `url(${bgfull})` }}
       >
+        <audio ref={audioRef} src="/backsound.mp3" autoPlay loop />
+        {/* Music control button */}
+        <div
+          className="fixed bottom-4 right-4 z-50"
+          title={isPlaying ? "Pause Music" : "Play Music"}
+        >
+          <button
+            onClick={togglePlayPause}
+            className="bg-white p-3 rounded-full shadow-lg hover:bg-gray-200 transition relative group"
+          >
+            {isPlaying ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            )}
+            {/* Tooltip */}
+            <span className="absolute opacity-0 group-hover:opacity-100 bg-black text-white text-sm px-2 py-1 rounded bottom-full mb-2 left-1/2 transform -translate-x-1/2 whitespace-nowrap">
+              {isPlaying ? "Pause Music" : "Play Music"}
+            </span>
+          </button>
+        </div>
+
         {/* Konten */}
         <div className="relative flex-grow">
           {/* Bagian Landing */}
@@ -123,7 +228,30 @@ export default function App() {
             className="bg-cover bg-center h-screen flex flex-col items-center justify-center text-center p-8 w-full"
             style={{ backgroundImage: `url(${bgImage})` }}
           >
-            <h2 className="text-sm text-gray-500 mb-2">THE WEDDING OF</h2>
+            <div className="w-full max-w-md bg-white rounded-t-full shadow-lg relative text-center">
+              <img
+                src="/chibi.PNG"
+                alt="Nofridho & Nadya"
+                className="w-full h-full object-cover"
+              />
+              {/* <h2 className="text-lg font-serif font-semibold mt-2">
+                AKAD NIKAH
+              </h2>
+              <p className="text-gray-600 mt-2 font-serif font-medium">
+                Senin, 03 Mei 2025
+              </p>
+              <p className="text-gray-600">07.00 WIB - Selesai</p>
+
+              <div className="mt-4 w-full flex justify-center">
+                <iframe
+                  className="w-60 h-40 rounded-lg shadow-md"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.671825365702!2d110.37881347413379!3d-7.821435277047214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57910b3c5d75%3A0xdeb4377e2d70c6d5!2sGrand%20Ballroom%20Hotel%20Santika!5e0!3m2!1sen!2sid!4v1618911111812!5m2!1sen!2sid"
+                  allowFullScreen
+                  loading="lazy"
+                ></iframe> 
+              </div> */}
+            </div>
+            <h2 className="text-sm text-gray-500 m-2">THE WEDDING OF</h2>
             <h1 className="text-5xl font-serif font-bold mb-4">
               NOFRIDHO & NADYA
             </h1>
@@ -136,13 +264,12 @@ export default function App() {
               Dengan hormat, kami mengundang Anda untuk menghadiri pernikahan
               kami.
             </p>
-            <div className="wrapper">
-              <button className="button" onClick={handleScroll}>
-                <svg className="svgIcon" viewBox="0 0 384 512">
-                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                </svg>
-              </button>
-            </div>
+            <button
+              onClick={handleScroll}
+              className="bg-white text-gray-800 px-6 py-2 rounded-full hover:bg-gray-300 transition"
+            >
+              Buka Undangan
+            </button>
           </div>
 
           {/* Undangan Section */}
@@ -150,22 +277,98 @@ export default function App() {
             {/* Bagian Ucapan */}
             <div className="h-screen flex flex-col items-center justify-center text-center p-8 relative">
               {/* Gambar dengan border lingkaran */}
-              <div className="w-80 h-180 rounded-full overflow-hidden relative">
-                <img
-                  src="/1.jpeg"
-                  alt="Nofridho & Nadya"
-                  className="w-full h-full object-cover"
-                />
-                {/* Teks ucapan di atas gambar dengan posisi tetap */}
-                <div className="absolute top-1/4 transform -translate-y-1/2 justify-center text-white">
-                  <h2 className="text-lg font-serif font-semibold mb-2 text-center">
-                    Assalamu'alaikum Wr. Wb
-                  </h2>
-                  <p className="text-sm text-center">
-                    Tanpa mengurangi rasa hormat, kami mengundang
-                    Bapak/Ibu/Saudara/i untuk hadir pada acara pernikahan kami.
-                  </p>
+              <div class="flex flex-col justify-center items-center min-h-screen">
+                <div class="bg-white rounded-3xl shadow-lg overflow-hidden max-w-lg w-full">
+                  <img
+                    src="/1.jpeg"
+                    alt="Mountain"
+                    class="w-full h-144 object-cover"
+                  />
+                  <div class="p-6">
+                    <p class="text-gray-700 leading-tight mb-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Aliquam eu sapien porttitor, blandit velit ac, vehicula
+                      elit. Nunc et ex at turpis rutrum viverra.
+                    </p>
+                    <h2 class="text-2xl font-bold text-gray-800 mb-2">
+                      Lorem, ipsum.
+                    </h2>
+                  </div>
                 </div>
+              </div>
+            </div>
+
+            {/* Bagian Ucapan */}
+            <div className="flex flex-col items-center justify-center text-center p-8 relative">
+              {/* Gambar dengan border lingkaran */}
+              <h3 className="text-xl font-serif font-bold mb-4">Salam</h3>
+              <div className="mb-12">
+                <div className="w-40 h-60 border border-white rounded-full overflow-hidden relative">
+                  <img
+                    src="/1.jpeg"
+                    alt="Nofridho & Nadya"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <h2 class="text-2xl font-bold text-gray-800 mb-2">
+                  Lorem, ipsum.
+                </h2>
+                <section class="flex justify-center items-center pt-4">
+                  <button
+                    href="/"
+                    class="group flex justify-center p-2 rounded-md drop-shadow-xl from-gray-800 bg-[#a21caf] text-white font-semibold hover:translate-y-3 hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1.2em"
+                      viewBox="0 0 24 24"
+                      stroke-width="1"
+                      fill="currentColor"
+                      stroke="currentColor"
+                      class="w-5"
+                    >
+                      <path d="M12.001 9C10.3436 9 9.00098 10.3431 9.00098 12C9.00098 13.6573 10.3441 15 12.001 15C13.6583 15 15.001 13.6569 15.001 12C15.001 10.3427 13.6579 9 12.001 9ZM12.001 7C14.7614 7 17.001 9.2371 17.001 12C17.001 14.7605 14.7639 17 12.001 17C9.24051 17 7.00098 14.7629 7.00098 12C7.00098 9.23953 9.23808 7 12.001 7ZM18.501 6.74915C18.501 7.43926 17.9402 7.99917 17.251 7.99917C16.5609 7.99917 16.001 7.4384 16.001 6.74915C16.001 6.0599 16.5617 5.5 17.251 5.5C17.9393 5.49913 18.501 6.0599 18.501 6.74915ZM12.001 4C9.5265 4 9.12318 4.00655 7.97227 4.0578C7.18815 4.09461 6.66253 4.20007 6.17416 4.38967C5.74016 4.55799 5.42709 4.75898 5.09352 5.09255C4.75867 5.4274 4.55804 5.73963 4.3904 6.17383C4.20036 6.66332 4.09493 7.18811 4.05878 7.97115C4.00703 9.0752 4.00098 9.46105 4.00098 12C4.00098 14.4745 4.00753 14.8778 4.05877 16.0286C4.0956 16.8124 4.2012 17.3388 4.39034 17.826C4.5591 18.2606 4.7605 18.5744 5.09246 18.9064C5.42863 19.2421 5.74179 19.4434 6.17187 19.6094C6.66619 19.8005 7.19148 19.9061 7.97212 19.9422C9.07618 19.9939 9.46203 20 12.001 20C14.4755 20 14.8788 19.9934 16.0296 19.9422C16.8117 19.9055 17.3385 19.7996 17.827 19.6106C18.2604 19.4423 18.5752 19.2402 18.9074 18.9085C19.2436 18.5718 19.4445 18.2594 19.6107 17.8283C19.8013 17.3358 19.9071 16.8098 19.9432 16.0289C19.9949 14.9248 20.001 14.5389 20.001 12C20.001 9.52552 19.9944 9.12221 19.9432 7.97137C19.9064 7.18906 19.8005 6.66149 19.6113 6.17318C19.4434 5.74038 19.2417 5.42635 18.9084 5.09255C18.573 4.75715 18.2616 4.55693 17.8271 4.38942C17.338 4.19954 16.8124 4.09396 16.0298 4.05781C14.9258 4.00605 14.5399 4 12.001 4ZM12.001 2C14.7176 2 15.0568 2.01 16.1235 2.06C17.1876 2.10917 17.9135 2.2775 18.551 2.525C19.2101 2.77917 19.7668 3.1225 20.3226 3.67833C20.8776 4.23417 21.221 4.7925 21.476 5.45C21.7226 6.08667 21.891 6.81333 21.941 7.8775C21.9885 8.94417 22.001 9.28333 22.001 12C22.001 14.7167 21.991 15.0558 21.941 16.1225C21.8918 17.1867 21.7226 17.9125 21.476 18.55C21.2218 19.2092 20.8776 19.7658 20.3226 20.3217C19.7668 20.8767 19.2076 21.22 18.551 21.475C17.9135 21.7217 17.1876 21.89 16.1235 21.94C15.0568 21.9875 14.7176 22 12.001 22C9.28431 22 8.94514 21.99 7.87848 21.94C6.81431 21.8908 6.08931 21.7217 5.45098 21.475C4.79264 21.2208 4.23514 20.8767 3.67931 20.3217C3.12348 19.7658 2.78098 19.2067 2.52598 18.55C2.27848 17.9125 2.11098 17.1867 2.06098 16.1225C2.01348 15.0558 2.00098 14.7167 2.00098 12C2.00098 9.28333 2.01098 8.94417 2.06098 7.8775C2.11014 6.8125 2.27848 6.0875 2.52598 5.45C2.78014 4.79167 3.12348 4.23417 3.67931 3.67833C4.23514 3.1225 4.79348 2.78 5.45098 2.525C6.08848 2.2775 6.81348 2.11 7.87848 2.06C8.94514 2.0125 9.28431 2 12.001 2Z"></path>
+                    </svg>
+                    <span class="absolute opacity-0 group-hover:opacity-100 group-hover:text-gray-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
+                      Instagram
+                    </span>
+                  </button>
+                </section>
+              </div>
+              <div className="mb-4">
+                <div className="w-40 h-60 border border-white rounded-full overflow-hidden relative">
+                  <img
+                    src="/1.jpeg"
+                    alt="Nofridho & Nadya"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <h2 class="text-2xl font-bold text-gray-800 mb-2">
+                  Lorem, ipsum.
+                </h2>
+                <section class="flex justify-center items-center pt-4">
+                  <button
+                    href="/"
+                    class="group flex justify-center p-2 rounded-md drop-shadow-xl from-gray-800 bg-[#a21caf] text-white font-semibold hover:translate-y-3 hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1.2em"
+                      viewBox="0 0 24 24"
+                      stroke-width="1"
+                      fill="currentColor"
+                      stroke="currentColor"
+                      class="w-5"
+                    >
+                      <path d="M12.001 9C10.3436 9 9.00098 10.3431 9.00098 12C9.00098 13.6573 10.3441 15 12.001 15C13.6583 15 15.001 13.6569 15.001 12C15.001 10.3427 13.6579 9 12.001 9ZM12.001 7C14.7614 7 17.001 9.2371 17.001 12C17.001 14.7605 14.7639 17 12.001 17C9.24051 17 7.00098 14.7629 7.00098 12C7.00098 9.23953 9.23808 7 12.001 7ZM18.501 6.74915C18.501 7.43926 17.9402 7.99917 17.251 7.99917C16.5609 7.99917 16.001 7.4384 16.001 6.74915C16.001 6.0599 16.5617 5.5 17.251 5.5C17.9393 5.49913 18.501 6.0599 18.501 6.74915ZM12.001 4C9.5265 4 9.12318 4.00655 7.97227 4.0578C7.18815 4.09461 6.66253 4.20007 6.17416 4.38967C5.74016 4.55799 5.42709 4.75898 5.09352 5.09255C4.75867 5.4274 4.55804 5.73963 4.3904 6.17383C4.20036 6.66332 4.09493 7.18811 4.05878 7.97115C4.00703 9.0752 4.00098 9.46105 4.00098 12C4.00098 14.4745 4.00753 14.8778 4.05877 16.0286C4.0956 16.8124 4.2012 17.3388 4.39034 17.826C4.5591 18.2606 4.7605 18.5744 5.09246 18.9064C5.42863 19.2421 5.74179 19.4434 6.17187 19.6094C6.66619 19.8005 7.19148 19.9061 7.97212 19.9422C9.07618 19.9939 9.46203 20 12.001 20C14.4755 20 14.8788 19.9934 16.0296 19.9422C16.8117 19.9055 17.3385 19.7996 17.827 19.6106C18.2604 19.4423 18.5752 19.2402 18.9074 18.9085C19.2436 18.5718 19.4445 18.2594 19.6107 17.8283C19.8013 17.3358 19.9071 16.8098 19.9432 16.0289C19.9949 14.9248 20.001 14.5389 20.001 12C20.001 9.52552 19.9944 9.12221 19.9432 7.97137C19.9064 7.18906 19.8005 6.66149 19.6113 6.17318C19.4434 5.74038 19.2417 5.42635 18.9084 5.09255C18.573 4.75715 18.2616 4.55693 17.8271 4.38942C17.338 4.19954 16.8124 4.09396 16.0298 4.05781C14.9258 4.00605 14.5399 4 12.001 4ZM12.001 2C14.7176 2 15.0568 2.01 16.1235 2.06C17.1876 2.10917 17.9135 2.2775 18.551 2.525C19.2101 2.77917 19.7668 3.1225 20.3226 3.67833C20.8776 4.23417 21.221 4.7925 21.476 5.45C21.7226 6.08667 21.891 6.81333 21.941 7.8775C21.9885 8.94417 22.001 9.28333 22.001 12C22.001 14.7167 21.991 15.0558 21.941 16.1225C21.8918 17.1867 21.7226 17.9125 21.476 18.55C21.2218 19.2092 20.8776 19.7658 20.3226 20.3217C19.7668 20.8767 19.2076 21.22 18.551 21.475C17.9135 21.7217 17.1876 21.89 16.1235 21.94C15.0568 21.9875 14.7176 22 12.001 22C9.28431 22 8.94514 21.99 7.87848 21.94C6.81431 21.8908 6.08931 21.7217 5.45098 21.475C4.79264 21.2208 4.23514 20.8767 3.67931 20.3217C3.12348 19.7658 2.78098 19.2067 2.52598 18.55C2.27848 17.9125 2.11098 17.1867 2.06098 16.1225C2.01348 15.0558 2.00098 14.7167 2.00098 12C2.00098 9.28333 2.01098 8.94417 2.06098 7.8775C2.11014 6.8125 2.27848 6.0875 2.52598 5.45C2.78014 4.79167 3.12348 4.23417 3.67931 3.67833C4.23514 3.1225 4.79348 2.78 5.45098 2.525C6.08848 2.2775 6.81348 2.11 7.87848 2.06C8.94514 2.0125 9.28431 2 12.001 2Z"></path>
+                    </svg>
+                    <span class="absolute opacity-0 group-hover:opacity-100 group-hover:text-gray-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
+                      Instagram
+                    </span>
+                  </button>
+                </section>
               </div>
             </div>
 
@@ -359,8 +562,18 @@ export default function App() {
 }
 
 {
+  /* <div className="wrapper">
+  <button className="button" onClick={handleScroll}>
+    <svg className="svgIcon" viewBox="0 0 384 512">
+      <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+    </svg>
+  </button>
+</div> */
+}
+
+{
   /* <div>
-                    <h1>Supabase Messages</h1>
+  <h1>Supabase Messages</h1>
                     <input
                       type="text"
                       placeholder="Name"
